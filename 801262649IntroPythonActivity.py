d734b3d77c4ec652204f3413ebff0fa4a35f3d79
LivingAshes <74620615+LivingAshes@users.noreply.github.com>
2022-09-15 22:43:00 -0400

    Delete 801262649IntroPythonActivity.py

--- 801262649IntroPythonActivity.py
+++
@@ -1,9 +0,0 @@
-#program 1
-def stringBothEnds(str):
-    if str.length < 2
-        return ""
-    return str[0:2] + str[-2:]  
-
-string1 = input("Please enter a string: /n")
-string2 = stringBothEnds(string1)
-print (string2)
